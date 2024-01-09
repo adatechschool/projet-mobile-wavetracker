7bb65d07ce017a4c60853ee1a4511dc017e182df
MargotHuet <margothuet4@gmail.com>
2024-01-09 16:30:02 +0100

    navigation between spotlists et spotdetails

--- app/src/main/java/com/example/wavetrackercompose/MainActivity.kt
+++ app/src/main/java/com/example/wavetrackercompose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.wavetrackercompose.ui.theme.WaveTrackerComposeTheme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -27,9 +28,11 @@ import androidx.compose.material3.Button
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 
 class MainActivity : ComponentActivity() {
@@ -38,12 +41,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             WaveTrackerComposeTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "spotlist") {
-                    composable("spotlist") {
-                        SpotList(
-                            detailsButtonClick = { navController.navigate("spotdetails") },
-                            userButtonClick = { navController.navigate("user") }) }
-                    composable("spotdetails") { SpotDetails(/*...*/) }
+                NavHost(navController = navController, startDestination = "spots") {
+                    composable("spots") {
+                        SpotList(Spots.spotsSample) { spotId: Int -> navController.navigate("spot/${spotId}") }
+                    }
+                    composable(
+                        "spot/{spotId}",
+                        arguments = listOf(navArgument("spotId") { type = NavType.IntType })
+                    ) { backStackEntry ->
+                        val spot = Spots.spotsSample.find { spot -> spot.id == backStackEntry.arguments?.getInt("spotId") }
+                        SpotDetails(spot!!)
+                    }
                     composable("user") { User(/*...*/) }
                     /*...*/
                 }
@@ -61,12 +69,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Spot(val name: String, val location: String)
+data class Spot(val id: Int, val name: String, val location: String)
 
 @Composable
-fun SpotCard(content: Spot) {
+fun SpotCard(content: Spot, detailsButtonClick: (Int) -> Unit) {
     // Add padding around our message
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+    Row(modifier = Modifier
+        .padding(all = 8.dp)
+        .clickable { detailsButtonClick(content.id) }) {
         Image(
             painter = painterResource(R.drawable.surfeur_grande_generation_blue_ocean_wave_ai),
             contentDescription = "surfer image",
@@ -107,42 +117,55 @@ fun SpotCard(content: Spot) {
 
 }
 
-@Preview
-@Composable
-fun PreviewSpotCard() {
-    SpotCard(
-        content = Spot("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
-}
-
 
 @Composable
-fun Spot(spots: List<Spot>) {
+fun SpotList(spots: List<Spot>, spotClick: (Int) -> Unit) {
     LazyColumn {
         items(spots) { spot ->
-            SpotCard(spot)
+            SpotCard(detailsButtonClick = spotClick, content = spot)
         }
     }
 }
 
 
+//@Composable
+//fun SpotList(detailsButtonClick: () -> Unit, userButtonClick: () -> Unit) {
+// Column {
+//   Button(onClick = detailsButtonClick) {
+//       Text(text = "Détails")
+//  }
+//
+//  Button(onClick = userButtonClick) {
+//      Text(text = "User")
+//    }
+// }
+//}
+
 @Composable
-fun SpotList(detailsButtonClick: () -> Unit, userButtonClick: () -> Unit) {
+fun SpotDetails(content: Spot) {
     Column {
-        Button(onClick = detailsButtonClick) {
-            Text(text = "Détails")
+        Text(
+            text = content.name,
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.titleMedium
+        )
+        // Add a vertical space between the author and message texts
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier
+                .padding(all = 4.dp),
+            tonalElevation = 1.dp,
+            shadowElevation = 1.dp
+        ) {
+            Text(
+                text = content.location,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
 
-        Button(onClick = userButtonClick) {
-            Text(text = "User")
-        }
     }
-
-}
-
-@Composable
-fun SpotDetails() {
-    Text(text = "Details")
 }
 
 @Composable
--- app/src/main/java/com/example/wavetrackercompose/Spots.kt
+++ app/src/main/java/com/example/wavetrackercompose/Spots.kt
@@ -3,22 +3,27 @@ package com.example.wavetrackercompose
 object Spots {
     val spotsSample = listOf(
     Spot(
+        1,
         "Rockaway Beach",
         "Tillamook, Oregon"
     ),
     Spot(
+        2,
         "Pipeline",
         "Oahu, Hawaii"
     ),
     Spot(
+        3,
         "Bubble",
         "Fuerteventura, Canary Islands"
     ),
     Spot(
+        4,
         "Pasta Point",
         "Maldives"
     ),
     Spot(
+        5,
         "Superbank",
         "Gold Cost, Australia"
     ),
